dcf50d64db8f3f002f8baa52f7073cc9af6b5a96
ayamprogramer <73216195+ayamprogramer@users.noreply.github.com>
2022-02-13 21:58:08 +0700

    Add files via upload

---
+++ tugas8-object.js
@@ -0,0 +1,16 @@
+function objek() {
+    var siswa = {
+        nama: 'maemunah',
+        kelas: '10',
+        alamat: 'Pasuruan',
+
+    }
+    console.log(siswa)
+    console.log()
+    siswa.kelas = '12'
+    siswa.nilai = 'A'
+    console.log(siswa)
+
+
+}
+objek()
